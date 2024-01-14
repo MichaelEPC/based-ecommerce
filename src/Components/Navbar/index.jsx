e608f42f0208ffc93d6130174727f8432bb2fb15
Michael Posada <finlmao@gmail.com>
2024-01-13 23:44:22 -0500

    Nav Design

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -3,39 +3,40 @@ import NavItems from "./NavItems"
 
 // Parte izquierda del navbar
 const left = [
-  { name: 'Shopi', to: '/', className: 'font-semibold text-xl' },
-  { name: 'All', to: '/' },
-  { name: 'Clothes', to: '/clothes' },
-  { name: 'Electronics', to: '/electronics' },
-  { name: 'Furnitures', to: '/furnitures' },
-  { name: 'Toys', to: '/toys' },
-  { name: 'Others', to: '/others' }
+  { name: 'YourStore', to: '/', className: 'font-semibold text-xl text-white mr-2' },
+  { name: 'All', to: '/', className: 'text-white font-normal' },
+  { name: 'Clothes', to: '/clothes', className: 'text-white font-normal' },
+  { name: 'Electronics', to: '/electronics',className: 'text-white font-normal' },
+  { name: 'Furnitures', to: '/furnitures', className: 'text-white font-normal' },
+  { name: 'Toys', to: '/toys', className: 'text-white font-normal' },
+{ name: 'Others', to: '/others', className: 'text-white font-normal' }
 ]
 
 // Parte derecha del navbar
 const right = [
-  { name: 'My Orders', to: '/my-orders', },
-  { name: 'My Account', to: '/my-account' },
-  { name: 'Sign-in', to: '/sing-in' },
+  { name: 'user@gmail.com', className: 'text-gray-200 cursor-none' },
+  { name: 'My Orders', to: '/my-orders', className: 'text-white font-normal' },
+  { name: 'My Account', to: '/my-account', className: 'text-white font-normal' },
+  { name: 'Sign-in', to: '/sing-in', className: 'text-white font-normal' },
 ]
 
 function Navbar() {
   return (
-    <nav className="bg-white flex justify-between items-center fixed z-10 w-full py-5 px-2 text-base top-0">
-      <ul className="flex items-center gap-3">
+    <nav className="bg-green-500 flex justify-between items-center fixed z-10 w-full py-5 px-2 text-base top-0">
+      <ul className="flex items-center gap-3 ">
         {left.map(({name, to, className}) => ( 
           <NavItems key={name} className={className} navbarName={name} to={to}/>
         ))}
       </ul>
       <ul className="flex items-center gap-3">
-        <li>
-        user@gmail.com
-        </li>
         {right.map(({name, to, className}) => ( 
           <NavItems key={name} className={className} navbarName={name} to={to}/>
         ))}
         <li>
-          💎
+          <div className="">
+          <img src="src/imgs/bolso-shopping-cuadrado-con-asa-sm.png" alt="" />
+          <div className="bg-white flex justify-center absolute rounded-full w-3 top-2 right-0"><p>2</p></div>
+          </div>
         </li>
       </ul>
     </nav>
